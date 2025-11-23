2597fe7649871b45fe00371e2ceaa78cd9299023
Layton <jihwan8807@naver.com>
2025-11-23 12:27:14 +0900

    feat(main): integrate React Query and routing with authentication
    - Added QueryClientProvider for React Query with custom default options.
    - Integrated RouterProvider for routing management.
    - Wrapped application in AuthProvider for authentication context.
    - Included Toaster for notifications and ReactQueryDevtools for development.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+import { router } from './router';
+import { AuthProvider } from './context/authContext.tsx';
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
+const devtools = import.meta.env.DEV ? (
+  <ReactQueryDevtools buttonPosition="bottom-left" initialIsOpen={false} />
+) : null;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            duration: 2000,
+          }}
+        />
+        <RouterProvider router={router} />
+        {devtools}
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
