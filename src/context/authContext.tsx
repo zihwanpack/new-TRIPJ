6ef5f64e2c1e7f7232b87642583db9b25f1db04b
Layton <jihwan8807@naver.com>
2025-11-27 21:12:00 +0900

    refactor(auth): streamline authentication context and methods
    - Removed accessToken state management from AuthContext for simplicity.
    - Updated authentication logic to use getAuthedUser and logoutApi for clarity.
    - Simplified the AuthProvider's return value by excluding the login method.

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,12 +1,9 @@
 import { createContext, useEffect, useState } from 'react';
 import type { User } from '../types/user.ts';
-import { tokenManager } from '../utils/tokenManager.ts';
-import { authAPI } from '../api/auth.ts';
+import { getAuthedUser, logoutApi } from '../api/auth.ts';
 
 type AuthContextType = {
   user: User | null;
-  accessToken: string | null;
-  login: (accessToken: string) => Promise<void>;
   logout: () => Promise<void>;
   loading: boolean;
 };
@@ -15,20 +12,15 @@ const AuthContext = createContext<AuthContextType | null>(null);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
-  const [accessToken, setAccessToken] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const authLogin = async () => {
       try {
-        const { accessToken, user } = await authAPI.me();
+        const { user } = await getAuthedUser();
         setUser(user);
-        setAccessToken(accessToken);
-        tokenManager.set(accessToken);
       } catch {
         setUser(null);
-        setAccessToken(null);
-        tokenManager.set(null);
       } finally {
         setLoading(false);
       }
@@ -36,25 +28,12 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     authLogin();
   }, []);
 
-  const login = async (accessToken: string) => {
-    setAccessToken(accessToken);
-    tokenManager.set(accessToken);
-    const { user } = await authAPI.me();
-    setUser(user);
-  };
-
   const logout = async () => {
-    await authAPI.logout();
-    setAccessToken(null);
+    await logoutApi();
     setUser(null);
-    tokenManager.set(null);
   };
 
-  return (
-    <AuthContext.Provider value={{ user, accessToken, login, logout, loading }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={{ user, logout, loading }}>{children}</AuthContext.Provider>;
 };
 
 export { AuthContext, AuthProvider };
