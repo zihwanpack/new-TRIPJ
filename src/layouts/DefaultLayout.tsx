c91598120816810387e3029564120e64f12fca89
Layton <jihwan8807@naver.com>
2025-11-27 21:13:27 +0900

    feat(layouts): introduce DefaultLayout component for consistent page structure
    - Added a new DefaultLayout component that includes Header, Footer, and an Outlet for nested routes.
    - Designed the layout to provide a flexible and responsive structure for pages.

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import { Footer } from './Footer.tsx';
+import { Header } from './Header.tsx';
+
+export const DefaultLayout = () => {
+  return (
+    <div className="flex min-h-dvh justify-center bg-slate-50">
+      <div className="mx-auto flex min-h-dvh w-full max-w-md flex-col justify-between gap-4 bg-white/95 shadow-xl shadow-slate-200/60">
+        <Header />
+        <main className="flex-1 overflow-y-auto">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
+};
