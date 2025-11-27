c59344a8f0de5bd805252dc869715c38297e9428
Layton <jihwan8807@naver.com>
2025-11-27 21:14:29 +0900

    refactor(layouts): update Header component to dynamically display titles based on route
    - Removed the static text prop from the Header component and implemented logic to set the header title based on the current route using useLocation.
    - Enhanced the Header component to improve user experience by providing context-specific titles for different pages.

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,15 +1,29 @@
 import { X } from 'lucide-react';
+import { useLocation } from 'react-router-dom';
 
-type HeaderText = '여행 추가하기' | '내 여행' | '이벤트 추가하기' | '마이페이지';
-
-type HeaderProps = {
-  text: HeaderText;
-};
-export const Header = ({ text }: HeaderProps) => {
+export const Header = () => {
+  const { pathname } = useLocation();
+  let headerTitle = '';
+  switch (pathname) {
+    case '/add-trip':
+      headerTitle = '여행 추가하기';
+      break;
+    case '/add-event':
+      headerTitle = '이벤트 추가하기';
+      break;
+    case '/my-trip':
+      headerTitle = '내 여행';
+      break;
+    case '/mypage':
+      headerTitle = '마이페이지';
+      break;
+    default:
+      headerTitle = '';
+  }
   return (
     <header className="">
       <X />
-      {text}
+      {headerTitle}
     </header>
   );
 };
