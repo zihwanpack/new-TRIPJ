9dff7dda86a7928ceb6f593408399e5f89d6593b
Layton <jihwan8807@naver.com>
2025-11-30 22:20:29 +0900

    refactor(layouts): remove HomeLayout component and update HomePage
    - Deleted the HomeLayout component to simplify the layout structure.
    - Enhanced HomePage to include user authentication and a dynamic header, improving user experience with personalized greetings.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,18 @@
+import { useAuth } from '../hooks/useAuth.tsx';
+import { Header } from '../layouts/Header.tsx';
+
 export const HomePage = () => {
-  return <section className="">123</section>;
+  const { user } = useAuth();
+
+  return (
+    <>
+      <Header title="홈" />
+      <section className="flex flex-col items-start m-3">
+        <div className="flex flex-col">
+          <p className="text-xl font-bold color-primary-base">여행자 {user?.nickname}님</p>
+          <p className="text-sm text-gray-500">3일 후 떠날 준비 되셨나요?</p>
+        </div>
+      </section>
+    </>
+  );
 };
