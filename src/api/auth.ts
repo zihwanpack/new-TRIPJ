64ef22173764fd776f2d9f834e0ec71c354b01f8
Layton <jihwan8807@naver.com>
2025-11-30 21:56:19 +0900

    refactor(auth): enhance API methods with type definitions
    - Renamed getAuthedUser to getAuthenticatedUserApi and added type definitions for GetAuthenticatedUserResponse and LogoutResponse.
    - Updated logoutApi to include type annotations for improved type safety and clarity in API responses.

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,11 +1,12 @@
+import type { GetAuthenticatedUserResponse, LogoutResponse } from '../types/auth.ts';
 import { httpClient } from './http/httpClient.ts';
 
-export const getAuthedUser = async () => {
-  const { data } = await httpClient.get('/auth/user');
+export const getAuthenticatedUserApi = async (): Promise<GetAuthenticatedUserResponse> => {
+  const { data } = await httpClient.get<GetAuthenticatedUserResponse>('/auth/user');
   return data;
 };
 
-export const logoutApi = async () => {
-  const { data } = await httpClient.post('/auth/logout');
+export const logoutApi = async (): Promise<LogoutResponse> => {
+  const { data } = await httpClient.post<LogoutResponse>('/auth/logout');
   return data;
 };
