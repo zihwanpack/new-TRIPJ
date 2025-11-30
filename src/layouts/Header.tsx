f6edf5c044f716d542ac4991313029b4eae45e1f
Layton <jihwan8807@naver.com>
2025-11-30 22:35:42 +0900

    refactor(layouts): simplify Header component by removing routing logic
    - Replaced dynamic header title logic with a static title prop for improved clarity and maintainability.
    - Updated the Header component structure for better readability and streamlined rendering.

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,29 +1,10 @@
 import { X } from 'lucide-react';
-import { useLocation } from 'react-router-dom';
 
-export const Header = () => {
-  const { pathname } = useLocation();
-  let headerTitle = '';
-  switch (pathname) {
-    case '/add-trip':
-      headerTitle = '여행 추가하기';
-      break;
-    case '/add-event':
-      headerTitle = '이벤트 추가하기';
-      break;
-    case '/my-trip':
-      headerTitle = '내 여행';
-      break;
-    case '/mypage':
-      headerTitle = '마이페이지';
-      break;
-    default:
-      headerTitle = '';
-  }
+export const Header = ({ title }: { title: string }) => {
   return (
-    <header className="">
-      <X />
-      {headerTitle}
+    <header className="sticky top-0 z-10 flex items-center bg-white pb-4 pt-2">
+      <X className="size-6 cursor-pointer" />
+      {title}
     </header>
   );
 };
