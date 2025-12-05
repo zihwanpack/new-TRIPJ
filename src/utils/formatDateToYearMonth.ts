33ddf349deeba31c4f9a99fe5185ec0aec468b9d
Layton <jihwan8807@naver.com>
2025-12-06 00:19:41 +0900

    fix(utils): update formatDateToYearMonth to correctly parse date strings
    - Modified the date parsing logic in formatDateToYearMonth to split the input date string using 'T' and then extract the year and month from the resulting array.
    - This change ensures accurate formatting of date strings in the expected format.

--- src/utils/formatDateToYearMonth.ts
+++ src/utils/formatDateToYearMonth.ts
@@ -1,4 +1,4 @@
 export const formatDateToYearMonth = (date: string) => {
-  const [year, month] = date.split('.');
+  const [year, month] = date.split('T')[0].split('-');
   return `${year}년 ${month}월`;
 };
