23879a7b4e0275f74b3a3fbac54858075e8e518f
Layton <jihwan8807@naver.com>
2025-12-23 21:35:47 +0900

    refactor: 홈화면 기존 여행 일자별 시간 계산 코드 삭제

--- src/utils/isPastTrip.ts
+++
@@ -1,6 +0,0 @@
-import dayjs from 'dayjs';
-
-export function isPastTrip(endDate: string): boolean {
-  const today = dayjs();
-  return dayjs(endDate).isBefore(today);
-}
--- src/utils/isUpcomingTrip.ts
+++
@@ -1,6 +0,0 @@
-import dayjs from 'dayjs';
-
-export function isUpcomingTrip(endDate: string): boolean {
-  const today = dayjs();
-  return dayjs(endDate).isAfter(today) || dayjs(endDate).isSame(today);
-}
