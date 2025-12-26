02f9306215787656ed05a5db5c6e0b17ab4d3e8e
Layton <jihwan8807@naver.com>
2025-12-27 03:50:35 +0900

    refactor: usePersistedStep 훅 삭제 후 범용적으로 storage 사용할 수 있도록 useStrage 훅 구현

---
+++ src/hooks/useStorage.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+type StorageType = 'session' | 'local';
+
+interface UseStorageProps<T> {
+  key: string;
+  initialValue: T;
+  type?: StorageType;
+}
+
+export const useStorage = <T,>({
+  key,
+  initialValue,
+  type,
+}: UseStorageProps<T>): { value: T; setValue: (value: T) => void; resetValue: () => void } => {
+  const storage = type === 'session' ? sessionStorage : localStorage;
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const storedData = storage.getItem(key);
+      if (storedData) {
+        return JSON.parse(storedData);
+      }
+      return storedData ? JSON.parse(storedData) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      storage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(error);
+    }
+  }, [key, value, storage]);
+
+  const resetValue = () => {
+    storage.removeItem(key);
+    setValue(initialValue);
+  };
+  return { value, setValue, resetValue };
+};
