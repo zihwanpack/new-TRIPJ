d8d76a897c38b8f8de42afc6e666c4cd5142a56e
Layton <jihwan8807@naver.com>
2026-01-15 16:52:04 +0900

    chore: useThrottle lint에러 수정

--- src/hooks/common/useThrottle.tsx
+++ src/hooks/common/useThrottle.tsx
@@ -3,17 +3,22 @@ import { useEffect, useRef, useState } from 'react';
 export const useThrottle = <T,>(value: T, delay: number = 1000): T => {
   const [throttledValue, setThrottledValue] = useState<T>(value);
 
-  const lastExecuted = useRef<number>(Date.now());
+  const lastExecuted = useRef<number | null>(null);
 
   useEffect(() => {
-    if (Date.now() >= lastExecuted.current + delay) {
-      lastExecuted.current = Date.now();
-      setThrottledValue(value);
+    const now = Date.now();
+    if (lastExecuted.current === null || now >= lastExecuted.current + delay) {
+      lastExecuted.current = now;
+      const timerId = setTimeout(() => {
+        setThrottledValue(value);
+      }, 0);
+      return () => clearTimeout(timerId);
     } else {
+      const remainingTime = delay - (now - lastExecuted.current);
       const timerId = setTimeout(() => {
         lastExecuted.current = Date.now();
         setThrottledValue(value);
-      }, delay);
+      }, remainingTime);
       return () => clearTimeout(timerId);
     }
   }, [value, delay]);
