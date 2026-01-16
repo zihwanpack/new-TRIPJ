f973b605d95726c12f6bb0fb0d7b57f1a958ae75
Layton <jihwan8807@naver.com>
2026-01-16 18:36:19 +0900

    chore: BaseLayout useOutlet을 써서  페이지 전환 애니메이션 적용 되도록 수정

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,17 +1,17 @@
 import { AnimatePresence } from 'framer-motion';
-import { Outlet, useLocation } from 'react-router-dom';
-import { PageTransition } from '../components/common/PageTransition.tsx';
+import { useLocation, useOutlet } from 'react-router-dom';
+import { PageTransition } from '../components/common/PageTransition';
 
 export const BaseLayout = () => {
   const location = useLocation();
+  const outlet = useOutlet();
+
   return (
     <div className="flex min-h-dvh justify-center bg-slate-50 dark:bg-slate-950">
       <div className="mx-auto flex min-h-dvh w-full max-w-md flex-col justify-between gap-4 bg-white/95 dark:bg-slate-900/95 shadow-xl shadow-slate-200/60 dark:shadow-slate-900/60">
-        <main className="flex-1 overflow-y-auto text-slate-900 dark:text-slate-100">
+        <main className="flex-1 overflow-hidden text-slate-900 dark:text-slate-100">
           <AnimatePresence mode="wait">
-            <PageTransition key={location.pathname}>
-              <Outlet />
-            </PageTransition>
+            <PageTransition key={location.pathname}>{outlet}</PageTransition>
           </AnimatePresence>
         </main>
       </div>
